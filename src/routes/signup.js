02ad0e502fe2560308430d03d6bef8a27566ac63
hudsonletuan <79324855+hudsonletuan@users.noreply.github.com>
2024-04-08 14:25:35 -0600

    assign api key for new user

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
+const ApiKey = require('../models/apiKeyModel');
 const bcrypt = require('bcryptjs');
 
 router.post('/', async (req, res) => {
@@ -16,6 +17,10 @@ router.post('/', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
+
+    const apiKey = {An GPT-3.5-Turbo API Key};
+    const newApiKey = new ApiKey({ username: newUser.username, apiKey: apiKey, modelName: 'gpt-3.5-turbo' });
+    await newApiKey.save();
     res.json({ message: 'Sign-up successful. You can now log in.' });
 });
 
